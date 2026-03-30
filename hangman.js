70f03aa9585bff0a60b02e57e0c8bc2861bdf081
Thalia999 <thaliac000@gmail.com>
2026-03-30 10:38:39 -0400

    fixed the image issue, built the clue string,

--- hangman.js
+++ hangman.js
@@ -7,11 +7,25 @@ let word= PossibleWords[randomIndex];
 console.log("Word Chosen :", word);
 
 
-
 let clueString = "";
 for (let i = 0; i < word.length; i++) {
+    var currentLetter = word.charAt(i);
+    if(guesses.indexOf(currentLetter) >= 0){
+clueString += currentLetter + " ";
+    }
+
+else{   
     clueString += "_ ";
+}
+
 }
 let clue = document.getElementById("clue");
 clue.textContent = clueString;
+}
+
+let guessLetter = function(){
+let input = document.getElementById("guess");
+let letter = input.value;
+guesses += letter;
+updatePage();
 }
