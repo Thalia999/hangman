488a4e94f4a49da6447a4285bcaafc780de0b354
Thalia999 <thaliac000@gmail.com>
2026-03-30 10:16:46 -0400

    created new game logic

--- hangman.js
+++ hangman.js
@@ -1,1 +1,16 @@
 const PossibleWords = ["obdurate", "versimilitude", "dissonant", "obsequious", "capitulate", "deleterious", "enervate", "gregarious"];
+
+let newGame = function(){
+
+var randomIndex =parseInt(Math.random() * PossibleWords.length);
+let word= PossibleWords[randomIndex];
+console.log("Word Chosen :", word);
+}
+
+
+let clueString = "";
+for (let i = 0; i < word.length; i++) {
+    clueString += "_ ";
+}
+let clue = document.getElementById("clue");
+clue.textContent = clueString;
